f705b54dd2c9c399800dfe94fa850f14dc7c8bea
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-01 19:28:20 +0530

    [Arjun] Add ; Added Performed

---
+++ Logical Programming/Program.cs
@@ -0,0 +1,29 @@
+﻿using LogicalProgramming;
+using System;
+
+namespace LogicalProgramming
+{
+    class LogicalProblem
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Logical series = new Logical();
+                        series.Fibonacci();
+                        break;
+
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
