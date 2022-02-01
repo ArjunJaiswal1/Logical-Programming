8256f32911c1e68edc3da3342eee517950017825
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-01 19:42:46 +0530

    [Arjun] Add ; Added Switched

--- Logical Programming/Logical.cs
+++ Logical Programming/Logical.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LogicalProgramming
+namespace LogicalProgramms
 {
     public class Logical
     {
@@ -28,7 +28,33 @@ namespace LogicalProgramming
                 Console.WriteLine(third);
             }
         }
+        public void PerfectNumber()
+        {
+            int sum = 0;
+            Console.WriteLine("Enter the number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                    Console.WriteLine("{0} ", i);
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine(num + " is a Perfect number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not a Perfect number");
+            }
+        }
     }
 }
+    
+
+
+
     
 
--- Logical Programming/Program.cs
+++ Logical Programming/Program.cs
@@ -1,16 +1,17 @@
 ﻿using LogicalProgramming;
+using LogicalProgramms;
 using System;
 
 namespace LogicalProgramming
 {
-    class LogicalProblem
+    class Logicalproblems
     {
         static void Main(string[] args)
         {
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Exit");
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect number\n 3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -19,7 +20,12 @@ namespace LogicalProgramming
                         series.Fibonacci();
                         break;
 
-                    case 2:
+                        case 2:
+                        LogicalProgramms.Logical num = new LogicalProgramms.Logical();
+                        num.PerfectNumber();
+                        break;
+
+                    case 3:
                         flag = false;
                         break;
                 }
