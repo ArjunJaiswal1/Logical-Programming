bdba23624b47c9badd13a29851b6367626c33882
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-01 19:56:25 +0530

    [Arjun] Add ; Added Switched

--- Logical Programming/Program.cs
+++ Logical Programming/Program.cs
@@ -11,7 +11,7 @@ namespace LogicalProgramming
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect number\n 3. prime number\n 4. Reverse number\n 5. Exit");
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect number\n 3. prime number\n 4. Reverse number\n 5. coupon number\n 6. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -36,6 +36,13 @@ namespace LogicalProgramming
                         break;
 
                     case 5:
+                        Console.Write("How many numbers you wants to generate :");
+                        int N = Convert.ToInt32(Console.ReadLine());
+                        Logical cn = new Logical();
+                        cn.CouponNumber(N);
+                        break;
+
+                    case 6:
                         flag = false;
                         break;
                 }
